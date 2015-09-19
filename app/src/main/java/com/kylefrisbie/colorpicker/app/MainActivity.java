48b9b126a50af1f16c5b45299672ed0eb07fecbc
kyle.l.frisbie@gmail.com <kyle.l.frisbie@gmail.com>
2015-09-19 17:05:14 -0600

    added color select button

--- app/src/main/java/com/kylefrisbie/colorpicker/app/MainActivity.java
+++ app/src/main/java/com/kylefrisbie/colorpicker/app/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 
@@ -45,13 +44,6 @@ public class MainActivity extends Activity {
         mColorBox = (ImageView)findViewById(R.id.imageView);
         resetBackgroundColor();
 
-        mColorBox.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                setColorPickResult();
-                return false;
-            }
-        });
         mRedButton = (NumberPicker)findViewById(R.id.numberPicker_Red);
         initializeNumberPicker(mRedButton);
 
